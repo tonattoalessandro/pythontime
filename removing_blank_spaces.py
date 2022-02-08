f5175293c03a49a8e134c953ad1caabb6d123b8b
tonattoalessandro <alessandro.tonatto@outlook.com>
2022-02-08 10:18:36 -0300

    Adding some comments in the code.

--- removing_blank_spaces.py
+++ removing_blank_spaces.py
@@ -1,3 +1,4 @@
+#This app shows how to remove blank spaces in strings, generally imputed by an user.
 name = "    Lincoln     "
 print(name)
 print(name.rstrip())
--- upper_and_lower.py
+++ upper_and_lower.py
@@ -1,3 +1,4 @@
+#This app shows how works some methods in strings.
 name = "Thomas"
 print(f"This is the name in uppercase: {name.upper()}.")
 print(f"This is the name in lowercase: {name.lower()}.")
