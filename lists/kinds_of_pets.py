c0efbdb617d0a8da52ecc783d55c803b2ebfc981
tonattoalessandro <alessandro.tonatto@outlook.com>
2022-02-09 18:17:19 -0300

    Exercise about lists and loops.

---
+++ lists/kinds_of_pets.py
@@ -0,0 +1,5 @@
+pets = ['dog', 'cat', 'fish']
+for pet in pets:
+    print(f"{pet.capitalize()} is a great pet option.")
+
+print("\nAny of this animals would be a great pet!")
---
+++ lists/pizzas_list.py
@@ -0,0 +1,5 @@
+pizzas = ['Calabresa', 'Mussarela', 'Bacon']
+for pizza in pizzas:
+    print(f"I like of {pizza.lower()} pizza.")
+
+print("\nI really love pizzas!")
