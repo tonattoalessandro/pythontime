43f38271b54809a263a111c32f5c7af79d6f5c07
tonattoalessandro <alessandro.tonatto@outlook.com>
2022-02-11 18:52:18 -0300

    Exercise about condition.

---
+++ conditional_content/alien_color_if.py
@@ -0,0 +1,4 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('You won 5 points!')
---
+++ conditional_content/amusement_park.py
@@ -0,0 +1,10 @@
+age = 12
+
+if age < 4:
+    cost = 0
+elif age < 18:
+    cost = 5
+else:
+    cost = 10
+
+print(f"Your admission cost is ${cost}")
